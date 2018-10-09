a9fd9f2adc77ff7bd47f98b73efbd94d377d98cb
Kevin Choi <mkchoi@gmail.com>
2018-10-09 10:42:12 +0800

    Initial Commit

---
+++ Invoice/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Invoice
+//
+//  Created by Kevin Choi on 10/9/18.
+//  Copyright © 2018 Kevin Choi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
